ccf61a20613befdfee21eeef98752be111a157c4
Osama Ennabati <oennabati3@gmail.com>
2024-11-07 17:25:32 +0100

    extra not completed

--- 9th task/assets/js/script.js
+++ 9th task/assets/js/script.js
@@ -108,5 +108,107 @@ function giveMeRandom(num) {
 
 console.log(giveMeRandom(10));
 
+//extra
+
+//esercizio 1
+
+function checkArray(array) {
+    let somma = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > 5) {
+            somma += array[i];
+            console.log(array[i] + " è maggiore di 5");
+        } else {
+            console.log(array[i] + " è minore di 5");
+        }
+    }
+    return somma + " è la somma dei numeri maggiori di 5";
+}
+
+console.log(checkArray(giveMeRandom(10)));
+
+//esercizio 2
+
+function shoppingCartTotal(shoppingCart) {
+    let total = 0;
+    for (let i = 0; i < shoppingCart.length; i++) {
+        total += shoppingCart[i].price * shoppingCart[i].quantity;
+    }
+    return "Il totale del carrello è"+ ' ' + total + '€';
+}
+
+
+const shoppingCart = [
+    { price: 299.99, name: 'AirPods', id: 1, quantity: 2 },
+    { price: 2.99, name: 'Sprite', id: 2, quantity: 3 },
+    { price: 1599.99, name: 'Iphone 16', id: 3, quantity: 1 }
+];
+
+console.log(shoppingCartTotal(shoppingCart)); 
+
+//esercizio 3
+
+function addItemsToItem(shoppingCart, itemId, numItemsToAdd) {
+    const item = shoppingCart.find(item => item.id === itemId);
+
+    return item.quantity += numItemsToAdd;
+
+}
+
+
+const updatedShoppingCart = addItemsToItem(shoppingCart, 1, 12);
+console.log(updatedShoppingCart);
+
+//esercizio 4
+
+function latestShoppingCart(shoppingCart) {
+    return shoppingCart[shoppingCart.length - 1];
+}
+
+
+const latestItem = latestShoppingCart(shoppingCart);
+console.log(latestItem);
+
+//esercizio 5
+
+//work in progress//
+
+//esercizio 6
+
+function average(array) {
+
+let sum = 0;
+let count = 0;
+
+for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+    sum += array[i];
+    count++;
+    }
+}
+
+if (count === 0) {
+    return 0;
+}
+return sum / count;
+}
+
+const numbers = [1, 9];
+console.log(average(numbers));
+
+//esercizio 8
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
